21a04ec3c85c97a267779dcec3056f261e681c11
Jonna Schutt <jonna.schutt@gmail.com>
2022-01-30 17:44:06 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Activity from "./components/Activity.jsx";
 
 function App() {
+  const [participants, setParticipants] = useState("none");
+  const [changeActivity, setChangeActivity] = useState(false)
+
+  let getParticipants = () => {
+    const myParticipants = document.querySelector('input[name="participants"]:checked').value;
+    setParticipants(myParticipants);
+    console.log(myParticipants)
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Choose participants: </h3>
+        <div>
+        <label htmlFor="participantsOne">1: </label>
+        <input type="radio" id="participants" name="participants" value="1"/>
+        </div>
+        <div>
+        <label htmlFor="participantsTwo">2: </label>
+        <input type="radio" id="participants" name="participants" value="2"/>
+        </div>
+        <div>
+        <label htmlFor="participantsFour">4: </label>
+        <input type="radio" id="participants" name="participants" value="4"/>
+        </div>
+        <div>
+        <label htmlFor="participantsNone">None: </label>
+        <input type="radio" id="participants" name="participants" value="none"/>
+        </div>
+        <div>
+          <button
+            onClick={() => {getParticipants(); setChangeActivity(!changeActivity)
+            }}
+          >
+            Find activity!
+          </button>
+          <Activity participants={participants} changeActivity={changeActivity}/>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Activity.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const Activity = ({ participants, changeActivity }) => {
+    const [activity, setActivity] = useState([]);
+
+    useEffect(async () => {
+        if(participants !== "none"){
+            let response = await fetch(`http://www.boredapi.com/api/activity?participants=${participants}`);
+            let json = await response.json();
+            setActivity(json);
+        } else {
+            let response = await fetch(`http://www.boredapi.com/api/activity/`);
+            let json = await response.json();
+            setActivity(json);
+        }
+    }, [changeActivity])
+
+  return (
+    <div>
+      <h4>{activity.activity}</h4>
+      <h5>Type: {activity.type}</h5>
+      <h5>Participants: {activity.participants}</h5>
+    </div>
+  );
+};
+
+export default Activity;
